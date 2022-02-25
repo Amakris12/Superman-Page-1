a430dff0d6dd1c7fd13422d662483d7452199cb5
Andrew Makris <amakri386@west-mec.org>
2022-02-24 20:42:20 -0700

    Started react Website

---
+++ sup/src/Pages/Home.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import Banner from '../Components/Banner'
+
+const Home = () => {
+  return (
+    <div>
+        <Banner/>
+        {/* Use this image */}
+        {/* https://www.enjpg.com/img/2020/superman-9.jpg */}
+    </div>
+  )
+}
+
+export default Home
